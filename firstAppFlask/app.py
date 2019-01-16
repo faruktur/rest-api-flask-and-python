87ba24ce38319de5a657b6c6389f1008ed52e67d
Faruk Tur <faruk.tur.b@gmail.com>
2019-01-16 20:58:54 +0300

    routing added

--- firstAppFlask/app.py
+++ firstAppFlask/app.py
@@ -1,14 +1,69 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/hello')
-def home():
-    return "Hello World"
+stores =[{
+    'name':'My Wonderful Store',
+    'items':[
+        {
+            'name':'My Item',
+            'price':15.99
+        }
+    ]
+}]
 
 @app.route('/')
-def home2():
-    return "Hello home World"
+def home():
+    return "Home Page"
 
+# POST - used to receive data
+# GET - used to send data back only
+
+#POST /store data : {name:}
+@app.route('/store',methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name' : request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+#GET /store/<string:name> data : {name:}
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name']==name:
+            return jsonify(store)
+    return jsonify({'message':'store not found'})
+
+#GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify(stores)
+
+
+#POST /store/<string:name>/item {name:,price:}
+@app.route('/store/<string:name>/item',methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name']==name
+        new_item={
+            'name':request_data['name'],
+            'price':request_data['price']
+        }
+        store['items'].append(new_item)
+        return jsonify(new_item)
+    return jsonify({'message':'store not found'})        
+
+#POST /store/<string:name>/item {name:,price:}
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    for store in stores:
+        if store['name']==name
+        return jsonify({'items':store.items})
+    return jsonify({'message':'store not found'})
 
 app.run(port=5000)
